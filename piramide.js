6056929608057f4bf569a72fdbf2e7e13b6b1113
danielaschvetz <danielamargarita14@gmail.com>
2023-05-07 20:00:00 -0300

    ejercicio1 tarea JS

---
+++ piramide.js
@@ -0,0 +1,20 @@
+//se crea una funcion 
+function piramide (numero) {
+    let numerosConcatenados="";
+  
+    for (let indice = 1; indice<=numero;indice++) {
+   
+   numerosConcatenados=numerosConcatenados+indice;
+    
+console.log(numerosConcatenados)
+        
+    
+    
+  }
+  
+   
+}
+
+//ejecucion de la funcion
+piramide(6);
+piramide(3);
